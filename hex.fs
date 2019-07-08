ac06fe448858fd53c364654825cc21fb299df419
nkone <phtruong@protonmail.com>
2019-07-07 17:57:09 -0700

    remove global vars, update comments

--- hex.fs
+++ hex.fs
@@ -5,8 +5,8 @@
 #                                                     +:+ +:+         +:+      #
 #    By: phtruong <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2019/07/07 13:10:05 by phtruong          #+#    #+#              #
-#    Updated: 2019/07/07 14:29:52 by phtruong         ###   ########.fr        #
+#    Created: 2019/07/07 17:56:14 by phtruong          #+#    #+#              #
+#    Updated: 2019/07/07 17:56:52 by phtruong         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,12 +18,12 @@ export const myFeature = defineFeature(function(context is Context, id is Id, de
     precondition
     {
         // Define the parameters of the feature type
-        annotation { "Name" : "Dia" }
+        annotation { "Name" : "Hex Diameter" }
         isLength(definition.hexDia, { (inch) : [ 0.01, 0.4375, 10] } as LengthBoundSpec);
         annotation { "Name" : "Height" }
         isLength(definition.hexHeight, { (inch) : [ 0.01, 0.265625, 10] } as LengthBoundSpec);
-        annotation { "Name" : "Inner dia" }
-        isLength(definition.innerDia, { (inch) : [ 0.01, 0.25, 10] } as LengthBoundSpec);
+        annotation { "Name" : "Hole diameter" }
+        isLength(definition.holeDia, { (inch) : [ 0.01, 0.25, 10] } as LengthBoundSpec);
         annotation { "Name" : "Pitch" }
         isLength(definition.pitch, { (inch) : [0.01, 0.05, 0.5] } as LengthBoundSpec);
         
@@ -31,137 +31,135 @@ export const myFeature = defineFeature(function(context is Context, id is Id, de
     }
     {
         // Extract units from user input
-        setVariable(context, "hex_dia", definition.hexDia/inch);
-        setVariable(context, "hex_height", definition.hexHeight/inch);
-        setVariable(context, "inner_dia", definition.innerDia/inch);
-        setVariable(context, "pitch", definition.pitch/inch);
+        var hexDia = definition.hexDia/inch;
+        var hexHeight = definition.hexHeight/inch;
+        var holeDia = definition.holeDia/inch;
+        var pitch = definition.pitch/inch;
         
         //Main steps
-        sketchHex(context, id);
-        extrudeBase(context, id);
-        sketchChamfer(context, id);
+        sketchHex(context, id, hexDia, holeDia);
+        extrudeBase(context, id, hexHeight);
+        sketchChamfer(context, id, hexDia, hexHeight);
         outerChamfer(context, id);
-        innerCylExtrude(context, id);
-        helixGuide(context,id);
-        innerSweep(context, id);
+        innerCylExtrude(context, id, hexHeight);
+        helixGuide(context, id, pitch);
+        innerSweep(context, id, pitch, holeDia);
         //Clear sketches
         deleteBodies(context, id + "clear", { "entities" : qUnion([qCreatedBy(id + "hex", EntityType.BODY),
-                                                                        qCreatedBy(id + "revolve_chamfer", EntityType.BODY),
+                                                                        qCreatedBy(id + "revolveChamfer", EntityType.BODY),
                                                                         qCreatedBy(id + "sweep", EntityType.BODY)])});
     });
     /*
     ** function sketchHex:
-    ** By using the law of equilateral triangle, we can find the height given one of the side (input from the user), 
-    ** the distance from the center to one of the vertex is 2/3 of the height of the triangle.
-    ** Thus by doing (hex_dia*sqrt(3))/2 * 2/3 or simplied to (hex_dia * sqrt(3)) / 3, 
-    ** we can get the distance from the center to the vertex for the sketch.
-	** Parameter:
-	** 		context is Context, id is Id (passed in from main)
+    ** Parameters:
+    ** [context] is Context (built in data structure)
+    ** [id] is Id (built in data struture)
+    ** [hexDia] is hex diameter
+    ** [holeDia] is the inner diameter of the hex
     ** Default unit: inch
     ** Functionality: Sketch the base for extrusion
-	** Return: NULL
+    ** Return: NULL
     */
-    function sketchHex(context is Context, id is Id)
+    function sketchHex(context is Context, id is Id, hexDia is number, holeDia is number)
     {
-        var hex_sketch = newSketch(context, id + "hex", {
+        var hexSketch = newSketch(context, id + "hex", {
                 "sketchPlane" : qCreatedBy(makeId("Top"), EntityType.FACE)
         });
-		// Hex diameter from user input
-        var hex_dia = getVariable(context, "hex_dia");
-		// Inner hole diamter from user input
-        var hole_dia = getVariable(context, "inner_dia");
-        var vertex_height = (hex_dia*sqrt(3))/3;
-        skRegularPolygon(hex_sketch, "nut", {
+        var vertexHeight = (hexDia*sqrt(3))/3;
+        skRegularPolygon(hexSketch, "nut", {
                 "center" : vector(0, 0) * inch,
-                "firstVertex" : vector(0, vertex_height) * inch,
+                "firstVertex" : vector(0, vertexHeight) * inch,
                 "sides" : 6
         });
-        skCircle(hex_sketch, "hole", {
+        skCircle(hexSketch, "hole", {
                 "center" : vector(0, 0) * inch,
-                "radius" : hole_dia/2 * inch
+                "radius" : holeDia/2 * inch
         });
-        skSolve(hex_sketch);
+        skSolve(hexSketch);
     }
     /*
     ** function extrudeBase:
     ** Using the sketch previously, extrude the the hex.
-	** Parameter:
-	** 		context is Context, id is Id (passed in from main)
+    ** Parameters:
+    ** [context] is Context (built in data structure)
+    ** [id] is Id (built in data struture)
+    ** [hexHeight] is height of the nut
     ** Default unit: inch
-    ** Functionality: Extrude the hex with the given height from user input.	
-	** Return: NULL
+    ** Functionality: Extrude the hex with the given height from user input.
+    ** Return: NULL
     */
-    function extrudeBase(context is Context, id is Id)
+    function extrudeBase(context is Context, id is Id, hexHeight is number)
     {
-		// height of the hex nut from user input
-        var height = getVariable(context, "hex_height");
-        opExtrude(context, id + "base_extrude", {
+        opExtrude(context, id + "baseExtrude", {
                 "entities" : qSketchRegion(id + "hex",true),
                 "direction" : evOwnerSketchPlane(context, {"entity" : qSketchRegion(id + "hex", true)}).normal,
                 "endBound" : BoundingType.BLIND,
-                "endDepth" : height * inch
+                "endDepth" : hexHeight * inch
         });
     }
     /*
     ** function sketchChamfer:
     ** Sketch two right triangles to chamfer the edge of the hex.
-	** Parameter:
-	** 		context is Context, id is Id (passed in from main)
+    ** Paramters:
+    ** [context] is Context (built in data structure)
+    ** [id] is Id (built in data struture)
+    ** [hexDia] is hex diameter
+    ** [hexHeight] is height of the nut
     ** Default unit: inch
     ** Functionality: Create sketch for revolve chamfer the hex.
 	** Return: NULL
     */
-    function sketchChamfer(context is Context, id is Id)
+    function sketchChamfer(context is Context, id is Id, hexDia is number, hexHeight is number)
     {
-        var chamfer_revolve = newSketch(context, id + "revolve_chamfer", {
+        var chamferRevolve = newSketch(context, id + "revolveChamfer", {
                 "sketchPlane" : qCreatedBy(makeId("Front"), EntityType.FACE)
         });
-        var vertex_x = getVariable(context, "hex_dia")/2;
-        var vertex_y = getVariable(context, "hex_height");
-        var offset = vertex_x/2;
-        skLineSegment(chamfer_revolve, "top_line", {
-                "start" : vector(vertex_x, vertex_y) * inch,
-                "end" : vector(vertex_x+offset, vertex_y) * inch
+        var vertexX = hexDia/2;
+        var vertexY = hexHeight;
+        var offset = vertexX/2;
+        skLineSegment(chamferRevolve, "topLine", {
+                "start" : vector(vertexX, vertexY) * inch,
+                "end" : vector(vertexX+offset, vertexY) * inch
         });
-        skLineSegment(chamfer_revolve, "top_right_line", {
-                "start" : vector(vertex_x+offset, vertex_y) * inch,
-                "end" : vector(vertex_x+offset, vertex_y-(offset/2)) * inch
+        skLineSegment(chamferRevolve, "topRightLine", {
+                "start" : vector(vertexX+offset, vertexY) * inch,
+                "end" : vector(vertexX+offset, vertexY-(offset/2)) * inch
         });
-        skLineSegment(chamfer_revolve, "top_hypo", {
-                "start" : vector(vertex_x, vertex_y) * inch,
-                "end" : vector(vertex_x+offset, vertex_y-(offset/2)) * inch
+        skLineSegment(chamferRevolve, "topHypo", {
+                "start" : vector(vertexX, vertexY) * inch,
+                "end" : vector(vertexX+offset, vertexY-(offset/2)) * inch
         });
-        skLineSegment(chamfer_revolve, "axis", {
+        skLineSegment(chamferRevolve, "axis", {
                 "start" : vector(0, 0) * inch,
                 "end" : vector(0, 1) * inch,
                 "construction" : true
         });
-        skLineSegment(chamfer_revolve, "bottom_line", {
-                "start" : vector(vertex_x, 0) * inch,
-                "end" : vector(vertex_x+offset, 0) * inch
+        skLineSegment(chamferRevolve, "bottomLine", {
+                "start" : vector(vertexX, 0) * inch,
+                "end" : vector(vertexX+offset, 0) * inch
         });
-        skLineSegment(chamfer_revolve, "bottom_right_line", {
-                "start" : vector(vertex_x+offset, 0) * inch,
-                "end" : vector(vertex_x+offset, offset/2) * inch
+        skLineSegment(chamferRevolve, "bottomRightLine", {
+                "start" : vector(vertexX+offset, 0) * inch,
+                "end" : vector(vertexX+offset, offset/2) * inch
         });
-        skLineSegment(chamfer_revolve, "bottom_hypo", {
-                "start" : vector(vertex_x, 0) * inch,
-                "end" : vector(vertex_x+offset, offset/2) * inch
+        skLineSegment(chamferRevolve, "bottomHypo", {
+                "start" : vector(vertexX, 0) * inch,
+                "end" : vector(vertexX+offset, offset/2) * inch
         });
-        skSolve(chamfer_revolve);
+        skSolve(chamferRevolve);
     }
     /*
     ** function outerChamfer
+    ** Paramters:
+    ** [context] is Context (built in data structure)
+    ** [id] is Id (built in data struture)
     ** Using the previous sketch, revolve remove the edges
-	** Parameter:
-	** 		context is Context, id is Id (passed in from main)
     ** Functionality: Chamfer the edge using revolve.
-	** Return: NULL
     */
     function outerChamfer(context is Context, id is Id)
     {
-        const axis = sketchEntityQuery(id + "revolve_chamfer", EntityType.EDGE, "axis");
-        var sketch = qSketchRegion(id + "revolve_chamfer");
+        const axis = sketchEntityQuery(id + "revolveChamfer", EntityType.EDGE, "axis");
+        var sketch = qSketchRegion(id + "revolveChamfer");
         revolve(context, id + "chamfer", {
             "operationType" : NewBodyOperationType.REMOVE,
             "entities" : sketch,
@@ -172,39 +170,41 @@ export const myFeature = defineFeature(function(context is Context, id is Id, de
     /*
     ** function innerCylExtrude
     ** Uses simple extrude on the surface on the inner hex to create a guide for next helix operation
-	** Parameter:
-	** 		context is Context, id is Id (passed in from main)
+    ** [context] is Context (built in data structure)
+    ** [id] is Id (built in data struture)
+    ** [hexHeight] is height of the nut
     ** Default unit: inch
     ** Functionality: Create a guide for helix for threading
-	** Return: NULL
+    ** Return: NULL
     */
-    function innerCylExtrude(context is Context, id is Id)
+    function innerCylExtrude(context is Context, id is Id, hexHeight is number)
     {
        
-        var edge = qNthElement(qCreatedBy(id + "base_extrude", EntityType.EDGE), 0);
-        var helix_height = getVariable(context, "hex_height")+0.15;
-        opExtrude(context, id + "inner_cyl", {
+        var edge = qNthElement(qCreatedBy(id + "baseExtrude", EntityType.EDGE), 0);
+        var helixHeight = hexHeight+0.15;
+        opExtrude(context, id + "innerCyl", {
                 "entities" : edge,
                 "direction" : evOwnerSketchPlane(context, {"entity" : qSketchRegion(id + "hex")}).normal,
                 "endBound" : BoundingType.BLIND,
-                "endDepth" : helix_height * inch
+                "endDepth" : helixHeight * inch
         });
     }
     /*
     ** function helixGuide
     ** Using the previous extrude, create a helix for threading
-	** Parameter:
-	** 		context is Context, id is Id (passed in from main)
+    ** Paramters:
+    ** [context] is Context (built in data structure)
+    ** [id] is Id (built in data struture)
+    ** [pitch] is thread pitch size
     ** Default unit: inch
     ** Functionality: Takes in user input of pitch size and create a helix
-	** Return: NULL
+    ** Return: NULL
     */
-    function helixGuide(context is Context, id is Id)
+    function helixGuide(context is Context, id is Id, pitch is number)
     {
-        var pitch = getVariable(context, "pitch");
-        helix(context, id + "thread_helix", {
+        helix(context, id + "threadHelix", {
                 "helixType" : HelixType.PITCH,
-                "entities" : qCreatedBy(id + "inner_cyl", EntityType.FACE),
+                "entities" : qCreatedBy(id + "innerCyl", EntityType.FACE),
                 "helicalPitch" : pitch * inch,
                 "startangle" : 0 * degree,
                 "handedness" :  Direction.CW,
@@ -214,36 +214,38 @@ export const myFeature = defineFeature(function(context is Context, id is Id, de
     /*
     ** function innerSweep
     ** Using the previous helixGuide, create and equilateral triangle to sweep remove the inner cylinder
-	** Parameter:
-	** 		context is Context, id is Id (passed in from main)
+    ** Parameters:
+    ** [context] is Context (built in data structure)
+    ** [id] is Id (built in data struture)
+    ** [pitch] is thread pitch size
+    ** [holeDia] is inner diameter of the nut
     ** Default unit: inch
-    ** Functionality: Remove sweep the inner hex nut
+    ** Functionality: Remove sweep the inner hex nut.
 	** Return: NULL
     */
-    function innerSweep(context is Context, id is Id)
+    function innerSweep(context is Context, id is Id, pitch is number, holeDia is number)
     {
-        var sweep_sketch = newSketch(context, id + "sweep", {
+        var sweepSketch = newSketch(context, id + "sweep", {
                 "sketchPlane" : qCreatedBy(makeId("Front"), EntityType.FACE)
         });
-        var vertex_x = getVariable(context, "inner_dia")/2;
-        var vertex_y = getVariable(context, "pitch")-0.01;
-        skLineSegment(sweep_sketch, "line1", {
-                "start" : vector(vertex_x, 0) * inch,
-                "end" : vector(vertex_x, -vertex_y) * inch
+        var vertexX = holeDia/2;
+        var vertexY = pitch-0.01;
+        skLineSegment(sweepSketch, "line1", {
+                "start" : vector(vertexX, 0) * inch,
+                "end" : vector(vertexX, -vertexY) * inch
         });
-        skLineSegment(sweep_sketch, "line2", {
-                "start" : vector(vertex_x, 0) * inch,
-                "end" : vector(vertex_x+((vertex_y*sqrt(3))/2), -vertex_y/2) * inch
+        skLineSegment(sweepSketch, "line2", {
+                "start" : vector(vertexX, 0) * inch,
+                "end" : vector(vertexX+((vertexY*sqrt(3))/2), -vertexY/2) * inch
         });
-        skLineSegment(sweep_sketch, "line3", {
-                "start" : vector(vertex_x, -vertex_y) * inch,
-                "end" : vector(vertex_x+((vertex_y*sqrt(3))/2), -vertex_y/2) * inch
+        skLineSegment(sweepSketch, "line3", {
+                "start" : vector(vertexX, -vertexY) * inch,
+                "end" : vector(vertexX+((vertexY*sqrt(3))/2), -vertexY/2) * inch
         });
-        skSolve(sweep_sketch);
-        sweep(context, id + "inner_sweep", {
+        skSolve(sweepSketch);
+        sweep(context, id + "innerSweep", {
                 "operationType" : NewBodyOperationType.REMOVE,
                 "profiles" : qSketchRegion(id + "sweep"),
-                "path" : qCreatedBy(id + "thread_helix", EntityType.EDGE)
+                "path" : qCreatedBy(id + "threadHelix", EntityType.EDGE)
         });
-    }
-
+    } 
